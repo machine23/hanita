32504ba043b6babd92792a30310f2cc0991963ce
Vassili Baranov <vassili.baranov@gmail.com>
2017-11-04 17:25:42 +0200

    форматирование utils.py

--- utils.py
+++ utils.py
@@ -11,13 +11,11 @@ def log(func):
     @functools.wraps(func)
     def inner(*args, **kwargs):
         msg = ", ".join(str(item) for item in args) if args else ""
-
         if kwargs:
             s_kwargs = ", ".join(
                 "{}={}".format(k, v) for k, v in kwargs.items()
             )
             msg += (", " if msg else "") + s_kwargs
-
         name = func.__name__
         logger.info("%s (%s)", name, msg)
         return func(*args, **kwargs)
