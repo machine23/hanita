776ef3190a31a2a396e7981e92f7eadf3a2a43ce
Vassili Baranov <vassili.baranov@gmail.com>
2017-11-04 22:10:59 +0200

    переформат функций

--- actions.py
+++ actions.py
@@ -7,39 +7,35 @@ MSG = "msg"
 actions_list = (PRESENCE, MSG)
 
 
-class Actions:
-    def __init__(self, user):
-        self.user = user
+def create_presence(user, status=None, timestamp=None):
+    """ Создает сообщение presence """
+    if timestamp is None:
+        timestamp = time.time()
+    msg = {
+        "action": PRESENCE,
+        "time": timestamp,
+        "type": "status",
+        "user": {
+            "account_name": user,
+            "status": status
+        }
+    }
+    return msg
 
-    def create_presence(self, status=None, timestamp=None):
-        """ Создает сообщение presence """
-        if timestamp is None:
-            timestamp = time.time()
+def create_msg(user, to_user, message, encoding="ascii", timestamp=None):
+    """ 
+    Создает простое сообщение msg.
+    Если to_user имеет префикс #, то это сообщение для группы
+    """
+    if timestamp is None:
+        timestamp = time.time()
+    if message:
         msg = {
-            "action": PRESENCE,
+            "action": MSG,
             "time": timestamp,
-            "type": "status",
-            "user": {
-                "account_name": self.user,
-                "status": status
-            }
+            "to": to_user,
+            "from": user,
+            "encoding": encoding,
+            "message": message
         }
         return msg
-
-    def create_msg(self, to_user, message, encoding="ascii", timestamp=None):
-        """ 
-        Создает простое сообщение msg.
-        Если to_user имеет префикс #, то это сообщение для группы
-        """
-        if timestamp is None:
-            timestamp = time.time()
-        if message:
-            msg = {
-                "action": MSG,
-                "time": timestamp,
-                "to": to_user,
-                "from": self.user,
-                "encoding": encoding,
-                "message": message
-            }
-            return msg
