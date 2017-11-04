90297e3444211ce35477948d475ab1acc64544ad
Vassili Baranov <vassili.baranov@gmail.com>
2017-11-04 18:14:24 +0200

    добавлен вспом. класс Actions

--- actions.py
+++ actions.py
@@ -1,3 +1,44 @@
-PRESENCE = "presence"
+import time
 
-actions_list = (PRESENCE, )
+
+PRESENCE = "presence"
+MSG = "msg"
+
+actions_list = (PRESENCE, MSG)
+
+
+class Actions:
+    def __init__(self, user):
+        self.user = user
+
+    def create_presence(self, status=None, timestamp=None):
+        """ Создает сообщение presence """
+        if timestamp is None:
+            timestamp = time.time()
+        msg = {
+            "action": PRESENCE,
+            "time": timestamp,
+            "type": "status",
+            "user": {
+                "account_name": self.user,
+                "status": status
+            }
+        }
+        return msg
+
+    def create_msg(self, to_user, message, encoding="ascii", timestamp=None):
+        """ 
+        Создает простое сообщение msg.
+        Если to_user имеет префикс #, то это сообщение для группы
+        """
+        if timestamp is None:
+            timestamp = time.time()
+        msg = {
+            "action": MSG,
+            "time": timestamp,
+            "to": to_user,
+            "from": self.user,
+            "encoding": encoding,
+            "message": message
+        }
+        return msg
