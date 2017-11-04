138768c1862fa8ea6fcf44775a7ea502bb0385c4
Vassili Baranov <vassili.baranov@gmail.com>
2017-11-04 17:24:18 +0200

    Реализация ротации лог-файлов

--- log_config.py
+++ log_config.py
@@ -1,4 +1,5 @@
 import logging
+from logging.handlers import TimedRotatingFileHandler
 
 
 logger = logging.getLogger("server.main")
@@ -6,7 +7,12 @@ logger = logging.getLogger("server.main")
 formatter = logging.Formatter(
     "%(asctime)s %(levelname)s\t%(module)s %(message)s")
 
-file_handler = logging.FileHandler("server.main.log")
+file_handler = TimedRotatingFileHandler(
+    "server.main.log",
+    when="D",
+    interval=1,
+    backupCount=3
+)
 file_handler.setLevel(logging.DEBUG)
 file_handler.setFormatter(formatter)
 
