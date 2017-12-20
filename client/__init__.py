eb44185e38dbc191f19bc97688157ecc143674bc
machine23 <vassili.baranov@gmail.com>
2017-12-20 19:28:04 +0200

    Чистка кода

--- client/__init__.py
+++ client/__init__.py
@@ -1,5 +1,5 @@
 """ Client module """
 from .client_connection import ClientConnection, ClientConnectionError
-from .client import Client, ClientUser, ClientError, QtClient
+from .client import Client, QtClient
 from .client_db import ClientDB, ClientDBError
 from .client_qtview import QtClientView
