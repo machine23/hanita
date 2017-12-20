b486b63902e8a28d8df541743bb99d1f96ebde6e
machine23 <vassili.baranov@gmail.com>
2017-12-20 20:00:39 +0200

    Исправление импортов

--- hanita_server/__init__.py
+++ hanita_server/__init__.py
@@ -1,4 +1,4 @@
-from .server import *
 from .models import *
+from .request_handler import ClientRequestHandler
+from .server import *
 from .server_db import *
-
--- hanita_server/server.py
+++ hanita_server/server.py
@@ -1,11 +1,5 @@
 """ server.py """
-import argparse
 import socketserver
-import sys
-from .server_db import ServerDB
-from .models import Base
-
-from .request_handler import ClientRequestHandler
 
 
 ###############################################################################
@@ -16,13 +10,8 @@ class Server(socketserver.ThreadingTCPServer):
     clients = {}
     allow_reuse_address = True
 
-    def __init__(
-        self,
-        server_address,
-        RequestHandlerClass,
-        base,
-        bind_and_activate=True
-    ):
+    def __init__(self, server_address, RequestHandlerClass,
+                 base, bind_and_activate=True):
         socketserver.ThreadingTCPServer.__init__(
             self, server_address, RequestHandlerClass, bind_and_activate)
         self.base = base
@@ -46,56 +35,3 @@ class Server(socketserver.ThreadingTCPServer):
         print("close_request")
         self.clients.pop(request, None)
         request.close()
-
-
-###############################################################################
-# read_args
-###############################################################################
-def read_args():
-    """ Читаем аргументы командной строки """
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "-a",
-        dest="addr",
-        default="127.0.0.1",
-        help="IP-адрес для прослушивания"
-    )
-    parser.add_argument(
-        "-p",
-        dest="port",
-        type=int,
-        default=7777,
-        help="TCP-порт (по умолчанию 7777)"
-    )
-
-    args = parser.parse_args()
-    return args
-
-
-###############################################################################
-# main
-###############################################################################
-def main():
-    """ mainloop """
-    args = read_args()
-
-
-    with Server((args.addr, args.port), ClientRequestHandler, Base) as server:
-        server_addr = server.socket.getsockname()
-        serve_message = "Serving on {host} port {port} ..."
-        print(serve_message.format(host=server_addr[0], port=server_addr[1]))
-
-        try:
-            server.serve_forever()
-        except KeyboardInterrupt:
-            pass
-
-    print("\nKeyboard interrupt received, exiting.")
-    sys.exit(0)
-
-
-###############################################################################
-#
-###############################################################################
-if __name__ == "__main__":
-    main()
