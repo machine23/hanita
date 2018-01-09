901ef7c1a238985c9c0042a1458a1bc537669d16
machine23 <vassili.baranov@gmail.com>
2018-01-09 14:05:24 +0200

    версия 0.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="Hanita",
-    version='0.1.1',
+    version='0.1.2',
     description='Simple messenger.',
     long_description='Hanita is simple messenger with multiusers chats.',
     url='https://github.com/machine23/hanita',
@@ -10,10 +10,24 @@ setup(
     keywords=['python', 'messenger', 'hanita'],
     author='Vassili Baranov',
     author_email='vassili.baranov@gmail.com',
-    packages=['hanita', 'hanita/forms', 'hanita_server', 'hanita_JIM'],
+    packages=[
+        'hanita',
+        'hanita/forms',
+        'hanita/forms/templates',
+        'hanita_server',
+        'hanita_server/avatars',
+        'hanita_JIM',
+    ],
+    package_data={
+        '': ['default_avatar.png', 'template.html'],
+    },
     include_package_data=True,
     python_requires='>=3.5',
     install_requires=[
-        'PyQt5==5.9', 'SQLAlchemy==1.1.15'
+        'PyQt5==5.9',
+        'SQLAlchemy==1.1.15',
+        'Jinja2==2.10',
+        'Pillow==5.0.0',
+        'PyOpenGL==3.1.0'
     ],
 )
